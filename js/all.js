589447005196ddb7a6bdbbeb7a7172020c08de7b
Jay <kred110396@gmail.com>
2020-08-29 10:50:15 +0800

    Delete all.js

--- js/all.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.l-header-bars').click(function (e) {
-        e.preventDefault();
-        $('.l-header-bars i').toggleClass('far fa-times');
-        $('.l-header-nav').toggleClass('active');
-    })
-})
